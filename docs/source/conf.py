1c7e24d5a936ce9bd475bd20749fef22fe4917c0
Zdenek Hynek <zdenek@syntensor.com>
2022-06-30 11:26:50 +0100

    Explicitly include static folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,11 +28,16 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
 # -- Options for HTML output
 
 html_theme = 'torchdyn_sphinx_theme'
 html_theme_options = {
-	'logo': '../_static/torchdyn_logo.svg',
+	'logo': '_static/torchdyn_logo.svg',
 }
 
 # -- Options for EPUB output
